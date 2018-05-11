3f8216210c9e51298979180ff55f94b4e2ae2398
Michel Perez <m1ch3lp3r3z@gmail.com>
2018-05-11 01:34:52 -0600

    Always pass an array

--- src/Service/BaseHttpService.php
+++ src/Service/BaseHttpService.php
@@ -47,7 +47,7 @@ abstract class BaseHttpService implements ContainerAccessorInterface
      */
     public function getRepository($name)
     {
-        return $this->_get($name, $this->_has($name) ? null : [
+        return $this->_get($name, [
             'client' => $this->client,
             'options' => [
                 'api_version' => $this->version
